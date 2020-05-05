a55c7c1e57ed5cb18175447d92fc4a5d6c87b458
Debug All <iDebugAll@gmail.com>
2020-05-05 20:58:50 +0300

    Make linter happier

--- generate_topology.py
+++ generate_topology.py
@@ -423,5 +423,6 @@ def good_luck_have_fun():
         write_topology_file(DIFF_DATA[2], dst='diff_topology.js')
         print(f'Для просмотра топологии с визуализацией изменений откройте файл diff_page.html')
 
+
 if __name__ == '__main__':
     good_luck_have_fun()
