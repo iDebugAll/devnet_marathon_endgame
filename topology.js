d6ec99e104e42a6bd668cb6a2e371ac847a14c0e
Debug All <iDebugAll@gmail.com>
2020-05-07 13:28:46 +0300

    Update sample topology

--- topology.js
+++ topology.js
@@ -127,7 +127,9 @@ var topologyData = {
             "model": "CSR1000V",
             "name": "dist-rtr02.devnet.lab",
             "serial_number": "9YZKNQKQ566",
-            "layerSortPreference": 7
+            "layerSortPreference": 7,
+            "primaryIP": "10.10.20.176",
+            "dcimDeviceLink": "http://localhost:32768/dcim/devices/?q=dist-rtr02.devnet.lab"
         },
         {
             "icon": "switch",
@@ -135,7 +137,9 @@ var topologyData = {
             "model": "Nexus9000 9000v Chassis",
             "name": "dist-sw01.devnet.lab",
             "serial_number": "9MZLNM0ZC9Z",
-            "layerSortPreference": 8
+            "layerSortPreference": 8,
+            "primaryIP": "10.10.20.177",
+            "dcimDeviceLink": "http://localhost:32768/dcim/devices/?q=dist-sw01.devnet.lab"
         },
         {
             "icon": "switch",
@@ -143,7 +147,9 @@ var topologyData = {
             "model": "Nexus9000 9000v Chassis",
             "name": "dist-sw02.devnet.lab",
             "serial_number": "93LCGCRUJA5",
-            "layerSortPreference": 8
+            "layerSortPreference": 8,
+            "primaryIP": "10.10.20.178",
+            "dcimDeviceLink": "http://localhost:32768/dcim/devices/?q=dist-sw02.devnet.lab",
         },
         {
             "icon": "router",
@@ -151,7 +157,9 @@ var topologyData = {
             "model": "n/a",
             "name": "core-rtr02.devnet.lab",
             "serial_number": "n/a",
-            "layerSortPreference": 5
+            "layerSortPreference": 5,
+            "primaryIP": "10.10.20.174",
+            "dcimDeviceLink": "http://localhost:32768/dcim/devices/?q=core-rtr02.devnet.lab",
         },
         {
             "icon": "router",
@@ -159,7 +167,9 @@ var topologyData = {
             "model": "CSR1000V",
             "name": "dist-rtr01.devnet.lab",
             "serial_number": "9S78ZRF2V2B",
-            "layerSortPreference": 7
+            "layerSortPreference": 7,
+            "primaryIP": "10.10.20.175",
+            "dcimDeviceLink": "http://localhost:32768/dcim/devices/?q=dist-rtr01.devnet.lab"
         },
         {
             "icon": "router",
@@ -167,15 +177,19 @@ var topologyData = {
             "model": "n/a",
             "name": "core-rtr01.devnet.lab",
             "serial_number": "n/a",
-            "layerSortPreference": 5
+            "layerSortPreference": 5,
+            "primaryIP": "10.10.20.173",
+            "dcimDeviceLink": "http://localhost:32768/dcim/devices/?q=core-rtr01.devnet.lab",
         },
         {
-            "icon": "unknown",
+            "icon": "router",
             "id": 6,
             "model": "CSR1000V",
             "name": "internet-rtr01.virl.info",
             "serial_number": "9LGWPM8GTV6",
-            "layerSortPreference": 1
+            "layerSortPreference": 1,
+            "primaryIP": "10.10.20.181",
+            "dcimDeviceLink": "http://localhost:32768/dcim/devices/?q=internet-rtr01.virl.info",
         },
         {
             "icon": "switch",
@@ -183,7 +197,9 @@ var topologyData = {
             "model": "IOSv",
             "name": "edge-sw01.devnet.lab",
             "serial_number": "927A4RELIGI",
-            "layerSortPreference": 3
+            "layerSortPreference": 3,
+            "primaryIP": "10.10.20.172",
+            "dcimDeviceLink": "http://localhost:32768/dcim/devices/?q=edge-sw01.devnet.lab",
         }
     ]
 };
